b086e5234efd1a618bcec192e5c3206464937246
Zhorik Zeniuk <Zhorik.me@list.ru>
2023-10-27 11:31:25 +0200

    created model Secret

--- safe_secret/models.py
+++ safe_secret/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Secret(models.Model):
+    hash = models.CharField(max_length=64, verbose_name='хэш')
+    ciphertext = models.TextField(verbose_name='зашифрованный текст')
+    link = models.CharField(max_length=250, verbose_name='ссылка')
+    lifetime = models.PositiveSmallIntegerField(verbose_name='время жизни')
+    code_frase = models.BooleanField(default=True, **NULLABLE)
+
+    def __str__(self):
+        return f'Секрет {self.pk}'
+
+    class Meta:
+        verbose_name = 'секрет'
+        verbose_name_plural = 'секреты'
