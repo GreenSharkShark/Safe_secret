3ba36073ea8332f511712e5b396d78cb106421b7
Zhorik Zeniuk <Zhorik.me@list.ru>
2023-10-31 11:59:27 +0100

    changes in model

--- safe_secret/models.py
+++ safe_secret/models.py
@@ -6,7 +6,7 @@ NULLABLE = {'null': True, 'blank': True}
 
 class Secret(models.Model):
     code_phrase = models.CharField(max_length=64, verbose_name='хэш', **NULLABLE)
-    ciphertext = models.TextField(verbose_name='зашифрованный текст')
+    secret_text = models.TextField(verbose_name='секретный текст')
     link = models.CharField(max_length=250, verbose_name='ссылка', **NULLABLE)
     lifetime = models.PositiveSmallIntegerField(verbose_name='время жизни')
     is_code_phrase = models.BooleanField(default=False)
